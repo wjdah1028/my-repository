9eb5aaae52aeb51b56cde90f4a368d13d7f684e6
wjdah <wjdah1028@naver.com>
2025-12-03 14:07:50 +0900

    :memo:Docs:println 추가

--- src/mygit/MyClass.java
+++ src/mygit/MyClass.java
@@ -3,6 +3,6 @@ package mygit;
 public class MyClass {
     public static void main(String args[]) {
         System.out.println("Hello Git!");
+        System.out.println("2nd version");
     }
 }
-sf
